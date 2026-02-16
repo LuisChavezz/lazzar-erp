2d8cb86c8b6de7178875d140a167012a6e4d88ce
LuisChavezz <ing.luischavezz@gmail.com>
2026-02-16 13:14:17 -0600

    feat(auth): remove registration feature

--- src/features/auth/services/actions.ts
+++ src/features/auth/services/actions.ts
@@ -1,5 +1,4 @@
 import { v1_api } from "@/src/api/v1.api";
-import { OnSuccessRegisterResponse, RegisterFormValues } from "../interfaces/register.interface";
 
 
 export const login = async (email: string, password: string) => {
@@ -8,10 +7,5 @@ export const login = async (email: string, password: string) => {
     email,
     password,
   });
-  return response.data;
-};
-
-export const register = async (data: RegisterFormValues): Promise<OnSuccessRegisterResponse | { message: string }> => {
-  const response = await v1_api.post("/onboarding/register/", data);
   return response.data;
 };
